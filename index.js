18ece93b03c52fd998619bc1667d0a61e230e14c
Adithya Nair <adithyanair121@gmail.com>
2025-02-10 12:31:58 +0530

    Ignore the thing

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-  if (details.url.startsWith("https://www.instagram.com/reels/")) {
+  if (details.url.href("https://www.instagram.com/reels/")) {
+    "https://www.instagram.com/reel/DFRrWTaIlUM/?igsh=ZXNvemd2N21peGF6"
+  }
+  else if (details.url.startsWith("https://www.instagram.com/reels/")) {
     let newUrl = details.url.replace("/reels/", "/reel/");
     chrome.tabs.update(details.tabId, { url: newUrl });
   }
