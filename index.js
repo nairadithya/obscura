1a0b0dcac0e484c1aa781dc10ce8b3eaa106912d
Adithya Nair <adithyanair121@gmail.com>
2025-02-10 12:35:22 +0530

    Added check for reels tab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-  if (details.url.href("https://www.instagram.com/reels/")) {
-    "https://www.instagram.com/reel/DFRrWTaIlUM/?igsh=ZXNvemd2N21peGF6"
+  if (details.url.href === "https://www.instagram.com/reels/" || details.url.href === "https://www.instagram.com/reels/?=next" ) {
+    let newUrl = "https://www.instagram.com/reel/DFRrWTaIlUM/?igsh=ZXNvemd2N21peGF6"
+    chrome.tabs.update(details.tabId, { url: newUrl });
   }
   else if (details.url.startsWith("https://www.instagram.com/reels/")) {
     let newUrl = details.url.replace("/reels/", "/reel/");
