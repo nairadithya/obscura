65ee7630a350bc64029628e973874a66aa39f77b
Adithya Nair <adithyanair121@gmail.com>
2025-04-08 19:12:15 +0530

    This seems to be working?

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,22 @@
-export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+export default defineBackground({
+    persistent: true,
+    type: "module",
+    main() {
+        const browserAPI = typeof browser !== "undefined" ? browser : chrome;
+        browserAPI.webNavigation.onHistoryStateUpdated.addListener(
+            (details) => {
+                if (
+                    details.url.startsWith(
+                        "https://www.instagram.com/reels/",
+                    ) &&
+                    details.url.length >
+                        "https://www.instagram.com/reels/?next=%2f".length
+                ) {
+                    const newUrl = details.url.replace("/reels/", "/reel/");
+                    browserAPI.tabs.update(details.tabId, { url: newUrl });
+                }
+            },
+            { url: [{ hostSuffix: "instagram.com" }] },
+        );
+    },
 });
--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,16 @@
+import "./hiders.css";
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  matches: ["*://*.instagram.com/*"],
+  main(ctx) {
+    document.addEventListener("DOMContentLoaded", (event) => {
+      element = document.querySelectorAll(
+        "a[href='https://aistudio.instagram.com/?utm_source=ig_web_nav']",
+      )[0];
+      if (element) {
+        element.remove();
+      }
+      console.log("Hello content.");
+    });
   },
 });
