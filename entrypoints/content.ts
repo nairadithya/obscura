ef41f4e8f0921b51e99ebabeea56d29e78973d71
Adithya Nair <adithyanair121@gmail.com>
2025-04-09 16:01:01 +0530

    Semi-working, need to figure out deleting logic

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -6,20 +6,47 @@ export default defineContentScript({
     function removeAllElementsAfterSuggestedPosts() {
       const divs = document.querySelectorAll("div");
       let suggestedPostsDiv = null;
-      for (const div of allDivs) {
+      for (const div of divs) {
         if (
           div.textContent &&
-          div.textContent.toLowerCase().includes("suggested posts")
+          div.textContent.toLowerCase() == "suggested posts"
         ) {
           suggestedPostsDiv = div;
           console.log("HAHA I FOUND YOU SUGGESTED");
+          console.log(suggestedPostsDiv);
           break;
         }
       }
+
+      if (suggestedPostsDiv) {
+        let parentNode = suggestedPostsDiv.parentNode;
+
+        let foundDiv = false;
+        const nodesToRemove = [];
+
+        for (const item of parentNode.children) {
+          if (item === suggestedPostsDiv) {
+            foundDiv = true;
+            continue;
+          }
+
+          if (foundDiv) {
+            nodesToRemove.push(item);
+          }
+        }
+        for (const item in nodesToRemove) {
+          item.remove();
+        }
+      }
     }
-    document.addEventListener("DOMContentLoaded", (event) => {
-      console.log("HAHAI FOUND YOU SUGGESTED");
+    const observer = new MutationObserver((mutations) => {
       removeAllElementsAfterSuggestedPosts();
     });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    return () => {
+      observer.disconnect();
+    };
   },
 });
