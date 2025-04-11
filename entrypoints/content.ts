ab11a773e0d947089022da49455d9eac94f6e8df
Adithya Nair <adithyanair121@gmail.com>
2025-04-11 10:21:48 +0530

    Merging things cause wxt naming conventions don't work

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,20 +1,66 @@
 export default defineContentScript({
     matches: ["*://*.instagram.com/reel/*"],
     main() {
-        if (!location.pathname.startsWith("/reel/")) return;
-        console.log("Hello from content.ts");
-        const divs = document.querySelectorAll("div");
-        let morePostsFromDiv;
-        for (const div of divs) {
-            if (
-                div.textContent &&
-                div.textContent.startsWith("More posts from")
-            ) {
-                morePostsFromDiv = div;
-                morePostsFromDiv.remove();
-                console.log(morePostsFromDiv);
-                break;
+        function deleteReelsSuggestions() {
+            if (!location.pathname.startsWith("/reel/")) return;
+            console.log("Hello from content.ts");
+            const divs = document.querySelectorAll("div");
+            let morePostsFromDiv;
+            for (const div of divs) {
+                if (
+                    div.textContent &&
+                    div.textContent.startsWith("More posts from")
+                ) {
+                    morePostsFromDiv = div;
+                    morePostsFromDiv.remove();
+                    console.log(morePostsFromDiv);
+                    break;
+                }
+            }
+            return;
+        }
+        deleteReelsSuggestions();
+
+        function removeAllElementsAfterSuggestedPosts() {
+            const divs = document.querySelectorAll("div");
+            let suggestedPostsDiv = null;
+            for (const div of divs) {
+                if (
+                    div.textContent &&
+                    div.textContent.toLowerCase() == "suggested posts"
+                ) {
+                    suggestedPostsDiv = div;
+                    break;
+                }
+            }
+
+            if (suggestedPostsDiv) {
+                const parentNode = suggestedPostsDiv.parentNode.parentNode;
+
+                let foundDiv = false;
+                const nodesToRemove = [];
+
+                for (const item of parentNode.children) {
+                    if (item === suggestedPostsDiv.parentNode) {
+                        foundDiv = true;
+                        continue;
+                    }
+
+                    if (foundDiv) {
+                        item.style.display = "none";
+                    }
+                }
             }
         }
+
+        const observer = new MutationObserver((mutations) => {
+            removeAllElementsAfterSuggestedPosts();
+        });
+
+        observer.observe(document.body, { childList: true });
+
+        return () => {
+            observer.disconnect();
+        };
     },
 });
